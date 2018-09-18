f1a8261f6ed063087b467f3412bf6ad7755ea5ad
honest <1131328108@qq.com>
2018-09-18 17:08:41 +0800

    给以前基础的文件做了分类

--- middle/threading/start.py
+++ middle/threading/start.py
@@ -1,7 +1,11 @@
 import threading
+import time
 
 def thread_job():
-  print('This is an added Thread, number is %s'% threading.current_thread())
+  print('This is an added Thread 1, number is %s \n'% threading.current_thread())
+  for i in range(10):
+    time.sleep(0.1)
+  print('T1 finish\n')
 
 def main():
   add_thread = threading.Thread(target = thread_job)
