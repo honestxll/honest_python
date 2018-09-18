f4fc0842dfe02487707b3b699111d8f0278087f1
honest <1131328108@qq.com>
2018-09-18 15:02:44 +0800

    导入模块和自定义模块

--- import.py
+++ import.py
@@ -1,6 +1,8 @@
 import time
+import module.m1 as m1
+# 自己写的模块需要在那个模块里面放一个空的 __init__.py 文件
 # from time import localtime, time
 # from time imoprt *
 # import time as t
 
-print(time.localtime().tm_year)
+m1.output(time.localtime().tm_year)
---
+++ module/m1.py
@@ -0,0 +1,6 @@
+def output(data):
+  if type(data) is list:
+    for i in data:
+      print(i)
+  else:
+    print(data)
