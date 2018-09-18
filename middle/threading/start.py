93c6630a26ffe9e3dcf80b365482be0b92fc3833
honest <1131328108@qq.com>
2018-09-18 17:19:00 +0800

    两个线程同时运行争抢

--- middle/threading/start.py
+++ middle/threading/start.py
@@ -1,20 +1,36 @@
 import threading
 import time
 
-def thread_job():
-  print('This is an added Thread 1, number is %s \n'% threading.current_thread())
-  for i in range(10):
-    time.sleep(0.1)
-  print('T1 finish\n')
+
+def thread_job1():
+    print('This is an added Thread 1, number is %s \n' %
+          threading.current_thread())
+    for i in range(10):
+        print('T1', i)
+    print('T1 finish\n')
+
+
+def thread_job2():
+    for i in range(10):
+        print('T2', i)
+    print('T2 finish\n')
+
 
 def main():
-  add_thread = threading.Thread(target = thread_job)
-  add_thread.start()
-  print('done \n')
+    thread1 = threading.Thread(target=thread_job1)
+    thread2 = threading.Thread(target=thread_job2)
 
-  print(threading.active_count())
-  print(threading.enumerate())
-  print(threading.current_thread())
+    thread1.start()
+    # 如果想让 T1 这个线程执行完之后再执行下面的代码，可以用 join
+    # join 相当于增加了线程的优先级
+    # thread1.join()
+    thread2.start()
+    print('done \n')
+
+    # print(threading.active_count())
+    # print(threading.enumerate())
+    # print(threading.current_thread())
+
 
 if __name__ == '__main__':
-  main()
+    main()
